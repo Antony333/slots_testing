5f53e836f7a796d856380e3b467d91015096708f
a.mishchenko <a.mishchenko@a1qa.com>
2018-05-15 18:56:00 +0300

    java_wrapper: add imagedriver

---
+++ src/main/java/image_driver/ImageDriver.java
@@ -0,0 +1,35 @@
+package image_driver;
+
+import api_builders.ParserApi;
+import org.apache.http.HttpEntity;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+
+public class ImageDriver {
+    private ParserApi parserApi;
+
+    public ImageDriver() {
+        this.parserApi = new ParserApi();
+    }
+
+    public JSONObject findElement(File file, String elementName, String threshold) throws Exception {
+        HttpEntity findElementHttpEntity = parserApi.findElement(file, elementName, threshold);
+
+        String json = EntityUtils.toString(findElementHttpEntity, StandardCharsets.UTF_8);
+
+        return new JSONObject(json);
+
+    }
+
+    public JSONObject initThresholds(File file, String elements) throws Exception {
+        HttpEntity initThresholdsEntity = parserApi.initThresholds(file, elements);
+
+        String json = EntityUtils.toString(initThresholdsEntity, StandardCharsets.UTF_8);
+
+        return new JSONObject(json);
+
+    }
+}
